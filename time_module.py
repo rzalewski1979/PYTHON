3d0bcc2f3fc91b4170800c3c2d3e14b03e34b304
Rafal <rafzal@wp.pl>
2025-04-23 08:55:38 +0200

    time module

---
+++ time_module.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+date=datetime(2020,11,4,14,53)
+print(date.strftime("%Y/%m/%m %H:%M:%S"))
+print(date.strftime("%y/%B/%d %H:%M:%S %p"))
+print(date.strftime("%a,%Y %b %d"))
+print(date.strftime("%A,%Y %B %d"))
+print(date.strftime("Weekday: %w"))
+print(date.strftime("Day of the year: %j"))
+print(date.strftime("Week number of the year: %W"))
